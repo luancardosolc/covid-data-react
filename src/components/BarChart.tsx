64fee48d76976e2625e7b22c1efe3aba4079bbee
Luan Cardoso <luanleitecardoso@gmail.com>
2022-12-25 15:53:30 -0300

    Bar Chart: Cumulative data arrays (Fix)

--- src/components/BarChart.tsx
+++ src/components/BarChart.tsx
@@ -29,18 +29,22 @@ export default function BarChart(props: { continents: any; countries: any; locat
   console.log('LUAN cumulativeDeathData', cumulativeDeathData);
   // MISSING STEPS:
 
-  // Sort both array
+  // Sort both arrays
   // Logic for the data type switcher
   // Logic to get the top 10 on the chart only
   // Logic for the Quantity Selector
   // Logic to make the Quantity Selector change the data
 
   return (
-    <Chart
-      width={350}
-      height={250}
-      data={data}
-      gridlines={<GridlineSeries line={<Gridline direction="all" />} />}
-    />
+    <>
+      <Chart
+        width={350}
+        height={250}
+        data={data}
+        gridlines={<GridlineSeries line={<Gridline direction="all" />} />}
+      />
+      {/* Data type selector */}
+      {/* Quantity Selector */}
+    </>
   );
 }
