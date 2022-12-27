267345e13465342696c1486d84bf08e65d902b5d
Luan Cardoso <luanleitecardoso@gmail.com>
2022-12-27 07:37:33 -0300

    Reducing the covid data json sample file

--- src/api/services/covid.service.ts
+++ src/api/services/covid.service.ts
@@ -5,9 +5,9 @@ import axios from "axios"
 class CovidDataService {
   getAll() {
     // Live Data
-    // return http.get<LocationList>('owid-covid-data.json')
+    return http.get<LocationList>('owid-covid-data.json')
     // Local Data (public/owid-covid-data.json)
-    return axios.get<LocationList>('owid-covid-data.json')
+    // return axios.get<LocationList>('owid-covid-data.json')
   }
 }
 
